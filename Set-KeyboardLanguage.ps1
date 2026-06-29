47b9155d6c2b70a327ba58e1e77cd142a6181452
jacktheseb <112408699+jacktheseb@users.noreply.github.com>
2026-06-29 13:33:40 +0200

    Create Set-KeyboardLanguage.ps1

---
+++ Set-KeyboardLanguage.ps1
@@ -0,0 +1,22 @@
+$Title = "Set-KeyboardLanguage"
+$host.UI.RawUI.WindowTitle = $Title
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+
+$env:APPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Roaming"
+$env:LOCALAPPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Local"
+$Env:PSModulePath = $env:PSModulePath + ";C:\Program Files\WindowsPowerShell\Scripts"
+$env:Path = $env:Path + ";C:\Program Files\WindowsPowerShell\Scripts"
+
+$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Set-KeyboardLanguage.log"
+Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+
+Write-Host -ForegroundColor Green "Set keyboard language to de-DE"
+Start-Sleep -Seconds 5
+
+$LanguageList = Get-WinUserLanguageList
+
+$LanguageList.Add("de-DE")
+Set-WinUserLanguageList $LanguageList -Force
+
+Stop-Transcript
